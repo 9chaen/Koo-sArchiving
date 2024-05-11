51033a4f9a896f434f75444cbf4a8b2a85459916
9chaen <146635401+9chaen@users.noreply.github.com>
2024-05-11 21:57:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const paragraphs = document.querySelectorAll('p');
     const dot = document.querySelector('.dot');
 
-    function handleClick(event) {
-        paragraphs.forEach((paragraph, index) => {
-            paragraph.style.transition = `transform 1s, opacity 1s`; // 애니메이션 속도 설정
-            paragraph.style.transform = 'translateY(100px)'; // 아래로 슬라이드
-            paragraph.style.opacity = 0; // 투명도 설정
-        });
-        dot.style.transition = 'opacity 1s, top 1s'; // 애니메이션 속도 설정
-        dot.style.opacity = 1; // 투명도 설정
-        dot.style.top = '0'; // 화면 상단으로 이동
-    }
+   function handleClick(event) {
+    paragraphs.forEach((paragraph, index) => {
+        paragraph.style.transition = `transform 1s, opacity 1s`; // 애니메이션 속도 설정
+        paragraph.style.transform = 'translateY(100px)'; // 아래로 슬라이드
+        paragraph.style.opacity = 0; // 투명도 설정
+    });
+    dot.style.transition = 'opacity 1s, top 1s'; // 애니메이션 속도 설정
+    dot.style.opacity = 1; // 투명도 설정
+    dot.style.top = '0'; // 화면 상단으로 이동
+}
 
     function handleMouseMove(event) {
         dot.style.transition = 'top 0.1s, left 0.1s'; // 애니메이션 속도 설정
