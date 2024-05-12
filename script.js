f4eb20cccaf2736cac0a5bb5ee7d5141dcc24fcb
9chaen <146635401+9chaen@users.noreply.github.com>
2024-05-12 22:21:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.body.addEventListener("click", function() {
+    document.body.addEventListener("click", function(event) { // 이벤트 객체 전달
         const text = document.querySelector(".text");
         const dot = document.querySelector(".dot");
 
@@ -12,11 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // 빨간 작은 원을 커서 위치로 이동시키고 나타내기
         dot.style.display = "block";
-        moveDot(event.clientX, event.clientY);
+        moveDot(event.clientX, event.clientY); // 이벤트 객체의 좌표 전달
         
         // 커서가 움직일 때마다 빨간 작은 원을 따라다니게 함
         document.addEventListener("mousemove", function(event) {
-            moveDot(event.clientX, event.clientY);
+            moveDot(event.clientX, event.clientY); // 이벤트 객체의 좌표 전달
         });
     });
 });
@@ -25,6 +25,7 @@ function moveDot(x, y) {
     const dot = document.querySelector(".dot");
     const dotX = x; // 커서의 X 좌표를 가져와 dotX에 할당
     const dotY = y; // 커서의 Y 좌표를 가져와 dotY에 할당
-   dot.style.left = (dotX - 20) + "px";
-dot.style.top = (dotY + 20) + "px";
+
+    dot.style.left = (dotX - 20) + "px"; // 점의 가로 위치 조정
+    dot.style.top = (dotY + 20) + "px"; // 점의 세로 위치 조정
 }
